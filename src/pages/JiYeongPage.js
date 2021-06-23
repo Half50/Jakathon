b77bcd32253702cd13a4057010018f121f38eb5d
JiyoungHyun <shyunji023@gmail.com>
2021-06-23 18:47:51 +0900

    feat : project 이미지 수정

--- src/pages/JiYeongPage.js
+++ src/pages/JiYeongPage.js
@@ -4,21 +4,22 @@ import { Header, Footer } from '../component';
 import Memory from '../assets/Memory.svg';
 import Flat from '../assets/Flat.svg';
 import Talko from '../assets/Talko.svg';
+import Library from '../assets/Library.svg';
 import JIyoungGit from '../assets/JIyoungGit.svg';
 import JiyoungProfile from '../assets/JiyoungProfile.svg';
 import Container from '../component/jiyoung/Container';
 const projectData = [
 	{
 		id: '0',
-		image: Memory,
-		title: 'SOPT에 진심이다>??ㄱㄸㄲ',
-		desc: '8번의 세미나를 통해 전반적인 \n HTML, CSS, Javascript에 대해 학습하고 \n React 라이브러리를 활용한 \n 다양한 과제를 수행했습니다. \n\n 앞으로 있을 APPJAM을 잘 수행하기 위해 \n 웹파트 내부 \n 자바스크립트 스터디를 진행하며 \n 언어에 대한 이해도를 높였습니다. \n\n서버, 디자인파트 합동세미나에 참여하여 \n 다른 개발자, 디자이너와 \n소통하는 방법도 익혔습니다.',
+		image: Library,
+		title: 'SOPT에 진.심.',
+		desc: '8번의 세미나를 통해 전반적인 \n HTML, CSS, Javascript에 대해 학습하고 \n React 라이브러리를 활용한 \n 다양한 과제를 수행했습니다. \n\n 앞으로 있을 \\APPJAM\\을 잘 수행하기 위해 \n 웹파트 내부 \n \\자바스크립트 스터디\\를 진행하며 \n 언어에 대한 이해도를 높였습니다. \n\n서버, 디자인파트 \\합동세미나\\에 참여하여 \n 다른 개발자, 디자이너와 \n소통하는 방법도 익혔습니다.',
 	},
 	{
 		id: '1',
 		image: Memory,
 		title: "기억 무새를 기억하십니까?",
-		desc: "2021 솝커톤에서 완성한 \n두 마리의 기억무새,\n 유머무새와 \\진지무새가\\ 안내하는 \n 센스있는 소통을 위한 \n 멘트 추천 서비스 입니다 \n\n 기억무새 프로젝트의 프론트엔드 개발 \n\n#React.js #Javascript",
+		desc: "\\2021 솝커톤\\에서 완성한 \n두 마리의 기억무새,\n 유머무새와 진지무새가 안내하는 \n 센스있는 소통을 위한 \n 멘트 추천 서비스 입니다 \n\n 기억무새 프로젝트의 프론트엔드 개발 \n\n#React.js #Javascript",
 	},
 	{
 		id: '2',
